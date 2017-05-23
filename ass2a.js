91ff1515263d499f861e7c2bbd50e286ac76753d
shaquibkhan <connectshaquib@gmail.com>
2017-05-23 09:56:56 +0530

    commit
    commit

---
+++ ass2a.js
@@ -0,0 +1,6 @@
+//Fat Arrow function...
+var message = function (p1, p2) {
+    console.log(" " + (p1 + p2));
+};
+message('khan', 22);
+// message is invoked with arguments as string & number....
---
+++ ass2a.ts
@@ -0,0 +1,7 @@
+//Fat Arrow function...
+var message = (p1 , p2) =>  {
+	console.log(` ${p1 + p2 }`);
+}
+message('khan',22);
+// message is invoked with arguments as string & number....
+
